fc186ea6221c2788cb4928b82205add7e254db5d
Deepak Wilson <deepakwilson2018@gmail.com>
2026-06-18 19:04:23 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.py
@@ -0,0 +1,15 @@
+# Last updated: 6/18/2026, 7:04:20 PM
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num==1:
+            return False
+        total = 1
+        for i in range(2, int(num**0.5)+1):
+            if num % i == 0:
+                total = total + i
+                if i!= num//i:
+                    total= total+ num//i
+        if total == num:
+            return True
+        else:
+            return False
