3b7148b792596de8c1cbc6ccc19197ccf6a062bc
Deepak Wilson <deepakwilson2018@gmail.com>
2026-07-30 14:46:12 +0530

    File updated at 7/30/2026, 2:46:11 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -1,17 +1,16 @@
-# Last updated: 7/30/2026, 12:33:01 PM
+# Last updated: 7/30/2026, 2:46:11 PM
 1class Solution:
-2    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+2    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
 3        left=1
-4        right=max(piles)
-5        while left<right:
+4        right=max(nums)
+5        while (left<right):
 6            mid=(left+right)//2
 7            sum=0
-8            for i in piles:
+8            for i in nums:
 9                sum+=ceil(i/mid)
-10            if sum<=h:
+10            if sum<=threshold: 
 11                right=mid
 12            else:
-13                left= mid+1
-14        return right
+13                left=mid+1
+14        return left 
 15
-16        
