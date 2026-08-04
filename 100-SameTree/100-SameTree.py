8e2a70b3697033f069acac55f7fb215aa5ac4a57
Deepak Wilson <deepakwilson2018@gmail.com>
2026-08-04 14:36:51 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.py
@@ -0,0 +1,18 @@
+# Last updated: 8/4/2026, 2:36:45 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def balanced(p,q):
+            if not p and not q:
+                return True
+            if (p and not q) or (not p and q):
+                return False
+            if (p.val!=q.val):
+                return False
+            return balanced(p.left, q.left) and balanced(p.right, q.right)
+        return balanced(p,q)
